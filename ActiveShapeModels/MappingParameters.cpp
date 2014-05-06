fc48314836205eb02f0ed68e60044124f299df1b
Orthocenter <chenyuxin.mail@gmail.com>
2014-05-06 15:13:25 +0800

    add getMappingMatrix() & getTranslationMatrix() to MappingParameters

--- ActiveShapeModels/MappingParameters.cpp
+++ ActiveShapeModels/MappingParameters.cpp
@@ -16,3 +16,14 @@ void MappingParameters::caculateNewCoordinates(double x, double y, double &resX,
 	resX = x * SCosR - y * SSinR + translationX;
 	resY = x * SSinR + y * SCosR + translationY;
 }
+
+void MappingParameters::getMappingMatrix(cv::Mat &mappingMatrix){
+	double SCosR = scale * cos(rotation), SSinR = scale * sin(rotation);
+	mappingMatrix = (cv::Mat_<double>(2, 2) << SCosR, -SSinR,
+												SSinR, SCosR);
+}
+
+void MappingParameters::getTranslationMatrix(cv::Mat &translationMatrix){
+	translationMatrix = (cv::Mat_<double>(2, 1) << translationX,
+													translationY);
+}
--- ActiveShapeModels/MappingParameters.h
+++ ActiveShapeModels/MappingParameters.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <opencv2\core\core.hpp>
+
 class MappingParameters
 {
 public:
@@ -7,5 +10,8 @@ public:
 
 	double scale, rotation, translationX, translationY;
 	void caculateNewCoordinates(double x, double y, double &resX, double &resY);
+
+	void getMappingMatrix(cv::Mat &mappingMatrix);
+	void getTranslationMatrix(cv::Mat &translationMatrix);
 };
 
